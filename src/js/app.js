0127bb28b0abef88dfebc4c80cba19d99182d481
Titter00 <olo_wojtas@o2.pl>
2022-12-11 18:44:16 +0100

    implementing sticky nav with Intersection Observer API

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,24 @@
 "use strict";
+//sticky navigation
 
+const header = document.querySelector(".header");
+
+const stickyNav = (entries) => {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) header.classList.add("sticky");
+  else header.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+});
+headerObserver.observe(header);
+
+// revealSection
 const allSections = document.querySelectorAll(".section");
-const revealSection = function (entries, observer) {
+const revealSection = (entries, observer) => {
   const [entry] = entries;
   console.log(entry);
 
