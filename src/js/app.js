d8bba1a10cf88eb9ca9db3c99fe10839bfabb15c
Titter00 <olo_wojtas@o2.pl>
2022-12-11 17:03:32 +0100

    new sections

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+// "use strict";
+
+// const header = document.querySelector("header");
+
+// console.log(header);
+// const stickyHeader = () => {
+//   const scroll = window.scrollY;
+
+//   if (scroll > 0) {
+//     header.classList.add("active");
+//   } else {
+//     header.classList.remove("active");
+//   }
+// };
+
+// window.addEventListener("scroll", stickyHeader);
